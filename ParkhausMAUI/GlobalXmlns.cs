a9c606599552f22c8b7059a02b254068ae8630c2
Illia Hudz <tilte@YORUNAMI>
2026-02-12 14:32:49 +0100

    Add project files.

---
+++ ParkhausMAUI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ParkhausMAUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ParkhausMAUI.Pages")]
